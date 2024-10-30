b8a573bf3bf3b45a3c4e712c465c128255b69fce
Chris Esler <chris.esler@gmail.com>
2024-10-30 16:26:46 -0700

    feat(setup): husky, jest, commitlint, some formatting, etc

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,10 +3,10 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://aecd2313c9390947868536fc57f95184@o4507219060850688.ingest.us.sentry.io/4508213620768768",
+  dsn: 'https://aecd2313c9390947868536fc57f95184@o4507219060850688.ingest.us.sentry.io/4508213620768768',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,10 +2,10 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://aecd2313c9390947868536fc57f95184@o4507219060850688.ingest.us.sentry.io/4508213620768768",
+  dsn: 'https://aecd2313c9390947868536fc57f95184@o4507219060850688.ingest.us.sentry.io/4508213620768768',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
